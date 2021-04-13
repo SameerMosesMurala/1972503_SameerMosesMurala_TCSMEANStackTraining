457bc355eb8702e990dfbe1b560f1bc3e301b8ed
SameerMosesMurala <sameer.moses160895@gmail.com>
2021-04-12 19:48:10 -0500

    Socket Chatting IO

---
+++ Chatting_SocketIO/chat-socketio.js
@@ -0,0 +1,16 @@
+let app = require("express")();
+let http = require("http").Server(app);  
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=> {
+    socket.on("chat",(msg)=> {
+        console.log(msg);   
+    });
+    
+
+})
+http.listen(9090,()=>console.log('server running on port number 9090'));
